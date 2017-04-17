f48a5c6e0b587bd02ddfabafbdc54369d370dd82
Japaka90 <japaka@yandex.ru>
2017-04-17 10:46:19 +0300

    fixed get method (deleted try/catch)

--- src/common/helpfulFunctions.js
+++ src/common/helpfulFunctions.js
@@ -1,12 +1,7 @@
 export default class LocalStorage {
 
     get(key) {
-        try{
             return JSON.parse(localStorage.getItem(key));
-        }
-        catch(e){
-            return localStorage.getItem(key);
-        }
     }
 
     set(key, value) {
