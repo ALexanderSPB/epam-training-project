e9a4bd5907b4a2c422c79b6f51861a15d54d07ad
vika <vihka.spihka@gmail.com>
2017-04-18 11:15:08 +0300

    Deleted react-bootstrap, changed mainPage, deleted wrong inputGroup

--- src/mainPage/mainPage.js
+++ src/mainPage/mainPage.js
@@ -1,55 +1,43 @@
 import React, { Component } from 'react';
-import {Grid, Col, Button} from 'react-bootstrap';
-// import Header from '../common/header';
-// import Footer from '../common/footer';
-// import InputGroup from '../common/inputGroup';
+import { Link } from 'react-router';
+import Header from '../common/header';
+import Footer from '../common/footer';
 
 export default class MainPage extends Component {
 
     render() {
-
-        const loginFormData = [
-            {
-                className: 'loginForm',
-                labelText: 'Email: ',
-                type: 'text',
-                value: 0
-            },
-            {
-                className: 'loginForm',
-                labelText: 'Пароль: ',
-                type: 'password',
-                value: 0
-
-            }
-        ];
-
         return (
-            <Grid>
-            {/*<Header />*/}
+            <container>
+                <Header />
                 <section className="row">
-                    <Col xs={10} xsOffset={1} sm={8} smOffset={2} md={6} mdOffset={3}>
-                            <form>
-            {/*<InputGroup params={loginFormData} />*/}
-                                <Button bsStyle="success">Войти</Button>
-                            </form>
-                            <p>Нет аккаунта? <Button bsStyle="link" href="#">Зарегистрируйся</Button></p>
-                    </Col>
-                </section>
-                <section className="row">
-                    <Col xs={10} xsOffset="1" md={8} mdOffset="2">
+                    <div className="col-xs-10 col-xs-offset-1 col-md-9 col-md-offset-2">
                         <h2>Что такое MySchedule?</h2>
                         <p>
                             MySchedule - это информационный портал, который позволит вам узнать интересующее Вас расписание в 2 клика.
                         </p>
-                        <h3>Являешься представителем своего заведения?</h3>
-                        <p>Зарегистрируй себя и свое заведение, чтобы получить полный доступ к редактированию расписания.</p>
-                        <h3>Ты преподаватель?</h3>
-                        <p>Регистрируйся и делай пометки об отмене занятия.</p>
-                    </Col>
+                        <section>
+                            <h3>Являешься представителем своего заведения?</h3>
+                            <p>
+                                <Link to="/">Зарегистрируй</Link> себя и свое заведение, чтобы получить полный доступ к редактированию расписания.
+                            </p>
+                        </section>
+                        <section>
+                            <h3>Ты преподаватель?</h3>
+                            <p>
+                                <Link to="/">Регистрируйся</Link> и делай пометки об отмене занятия.
+                            </p>
+                        </section>
+                        <section>
+                            <h3>Просто учащийся?</h3>
+                            <p>
+                                Воспользуйся нашим сайтом, выбери учебное заведение ниже и узнай расписание. Все просто!
+                            </p>
+                            {/* --- There must be a component with dropdown list and schedule --- */}
+                        </section>
+                    </div>
                 </section>
-        {/*<Footer />*/}
-            </Grid>
-    );
+                <Footer />
+            </container>
+        );
     }
 }
