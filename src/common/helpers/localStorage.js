a4e1a5876752166fc367bca15caa7150907f8970
Japaka90 <japaka@yandex.ru>
2017-04-19 23:40:34 +0300

    added try/catch to get method

---
+++ src/common/helpers/localStorage.js
@@ -0,0 +1,15 @@
+export default class LocalStorage {
+
+    get(key) {
+        try{
+            JSON.parse(localStorage.getItem(key);
+        }
+        catch(e) {
+            return localStorage.getItem(key);
+        }
+    }
+
+    set(key, value) {
+        localStorage.setItem(key, JSON.stringify(value));
+    }
+};
