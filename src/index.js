528c3b7d599aea8176cbf456cdb07311bcdb11ee
vika <vihka.spihka@gmail.com>
2017-04-20 15:46:19 +0300

    Added page wrapper + changed object for input

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom';
 import {Router, Route, browserHistory} from 'react-router';
 import {syncHistoryWithStore} from 'react-router-redux';
 import {Provider} from 'react-redux';
+import Header from '../common/pageParts/header';
+import Footer from '../common/pageParts/footer';
 import LoginPage from './pages/loginPage/loginPage';
 import configureStore from './store/configureStore';
 import './index.css';
@@ -12,9 +14,13 @@ const history = syncHistoryWithStore(browserHistory, store);
 
 ReactDOM.render(
     <Provider store={store}>
-        <Router history={history}>
-            <Route path='login' component={LoginPage} />
-        </Router>
+        <div className="container">
+            <Header />
+            <Router history={history}>
+                <Route path='login' component={LoginPage} />
+            </Router>
+            <Footer />
+        </div>
     </Provider>,
     document.getElementById('root')
 );
--- src/pages/loginPage/loginPage.js
+++ src/pages/loginPage/loginPage.js
@@ -1,7 +1,5 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router';
-import Header from '../common/pageParts/header';
-import Footer from '../common/pageParts/footer';
 import Input from '../common/UI/input';
 
 export default class LoginPage extends Component {
@@ -10,48 +8,44 @@ export default class LoginPage extends Component {
 
         const loginFormData = [
             {
-                className: 'loginForm',
+                inputId: 'loginForm__email',
+                placeholder: 'email@smth.com',
                 labelText: 'Email: ',
-                type: 'text',
-                value: ''
+                type: 'text'
             },
             {
-                className: 'loginForm',
+                inputId: 'loginForm__password',
+                placeholder: 'Type password',
                 labelText: 'Пароль: ',
-                type: 'password',
-                value: ''
+                type: 'password'
             }
         ];
 
         return (
-            <container>
-                <Header />
-                <section className="row">
-                    <div className="col-xs-10 col-xs-offset-1 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
-                        <form>
-                            <Input
-                                className={loginFormData[0].className}
-                                labelText={loginFormData[0].labelText}
-                                type={loginFormData[0].type}
-                                value={loginFormData[0].value}
-                            />
-                            <Input
-                                className={loginFormData[1].className}
-                                labelText={loginFormData[1].labelText}
-                                type={loginFormData[1].type}
-                                value={loginFormData[1].value}
-                            />
-                            <button className="btn btn-success">Войти</button>
-                        </form>
-                        <p>Нет аккаунта?
-                            <button className="btn btn-link">
-                                <Link to="/">Зарегистрируйся</Link>
-                            </button>
-                        </p>
-                    </div>
-                </section>
-                <Footer />
-            </container>
+            <section className="row">
+                <div className="col-xs-10 col-xs-offset-1 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+                    <form>
+                        <Input
+                            inputId={loginFormData[0].inputId}
+                            placeholder={loginFormData[0].placeholder}
+                            labelText={loginFormData[0].labelText}
+                            type={loginFormData[0].type}
+                        />
+                        <Input
+                            inputId={loginFormData[1].inputId}
+                            placeholder={loginFormData[1].placeholder}
+                            labelText={loginFormData[1].labelText}
+                            type={loginFormData[1].type}
+                        />
+                        <button className="btn btn-success">Войти</button>
+                    </form>
+                    <p>Нет аккаунта?
+                        <button className="btn btn-link">
+                            <Link to="/">Зарегистрируйся</Link>
+                        </button>
+                    </p>
+                </div>
+            </section>
         );
     }
 }
