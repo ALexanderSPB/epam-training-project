b0cc7822420f629385da8f6fb45c50d59d49ec8c
vika <vihka.spihka@gmail.com>
2017-04-20 14:57:33 +0300

    Added page wrapper

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ import {Router, Route, browserHistory} from 'react-router';
 import {syncHistoryWithStore} from 'react-router-redux';
 import {Provider} from 'react-redux';
 import configureStore from './store/configureStore';
+import Header from '../common/pageParts/header';
+import Footer from '../common/pageParts/footer';
 import MainPage from './pages/mainPage/mainPage';
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap/dist/css/bootstrap-theme.css';
@@ -14,10 +16,14 @@ const history = syncHistoryWithStore(browserHistory, store);
 
 ReactDOM.render(
     <Provider store={store}>
-        <Router history={history}>
-            <Route path='/' component={MainPage}>
-            </Route>
-        </Router>
+        <div className="container">
+            <Header />
+            <Router history={history}>
+                <Route path='/' component={MainPage}>
+                </Route>
+            </Router>
+            <Footer />
+        </div>
     </Provider>,
     document.getElementById('root')
 );
--- src/pages/mainPage/mainPage.js
+++ src/pages/mainPage/mainPage.js
@@ -1,43 +1,37 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router';
-import Header from '../common/pageParts/header';
-import Footer from '../common/pageParts/footer';
 
 export default class MainPage extends Component {
 
     render() {
         return (
-            <container>
-                <Header />
-                <section className="row">
-                    <div className="col-xs-10 col-xs-offset-1 col-md-9 col-md-offset-2">
-                        <h2>Что такое MySchedule?</h2>
+            <section className="row">
+                <div className="col-xs-10 col-xs-offset-1 col-md-9 col-md-offset-2">
+                    <h2>Что такое MySchedule?</h2>
+                    <p>
+                        MySchedule - это информационный портал, который позволит вам узнать интересующее Вас расписание в 2 клика.
+                    </p>
+                    <section>
+                        <h3>Являешься представителем своего заведения?</h3>
                         <p>
-                            MySchedule - это информационный портал, который позволит вам узнать интересующее Вас расписание в 2 клика.
+                            <Link to="/">Зарегистрируй</Link> себя и свое заведение, чтобы получить полный доступ к редактированию расписания.
                         </p>
-                        <section>
-                            <h3>Являешься представителем своего заведения?</h3>
-                            <p>
-                                <Link to="/">Зарегистрируй</Link> себя и свое заведение, чтобы получить полный доступ к редактированию расписания.
-                            </p>
-                        </section>
-                        <section>
-                            <h3>Ты преподаватель?</h3>
-                            <p>
-                                <Link to="/">Регистрируйся</Link> и делай пометки об отмене занятия.
-                            </p>
-                        </section>
-                        <section>
-                            <h3>Просто учащийся?</h3>
-                            <p>
-                                Воспользуйся нашим сайтом, выбери учебное заведение ниже и узнай расписание. Все просто!
-                            </p>
-                            {/* --- There must be a component with dropdown list and schedule --- */}
-                        </section>
-                    </div>
-                </section>
-                <Footer />
-            </container>
+                    </section>
+                    <section>
+                        <h3>Ты преподаватель?</h3>
+                        <p>
+                            <Link to="/">Регистрируйся</Link> и делай пометки об отмене занятия.
+                        </p>
+                    </section>
+                    <section>
+                        <h3>Просто учащийся?</h3>
+                        <p>
+                            Воспользуйся нашим сайтом, выбери учебное заведение ниже и узнай расписание. Все просто!
+                        </p>
+                        {/* --- There must be a component with dropdown list and schedule --- */}
+                    </section>
+                </div>
+            </section>
         );
     }
 }
