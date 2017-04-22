850d36610e69ff15cdf2d789205e56ad53c3626e
vika <vihka.spihka@gmail.com>
2017-04-22 21:25:08 +0300

    Fixed page wrapper + routes in separate file

--- src/index.js
+++ src/index.js
@@ -1,29 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {Router, Route, browserHistory} from 'react-router';
-import {syncHistoryWithStore} from 'react-router-redux';
 import {Provider} from 'react-redux';
 import configureStore from './store/configureStore';
-import Header from '../common/pageParts/header';
-import Footer from '../common/pageParts/footer';
-import MainPage from './pages/mainPage/mainPage';
+import App from './pages/app';
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap/dist/css/bootstrap-theme.css';
 import './index.css';
 
 const store = configureStore();
-const history = syncHistoryWithStore(browserHistory, store);
 
 ReactDOM.render(
     <Provider store={store}>
-        <div className="container">
-            <Header />
-            <Router history={history}>
-                <Route path='/' component={MainPage}>
-                </Route>
-            </Router>
-            <Footer />
-        </div>
+        <App />
     </Provider>,
     document.getElementById('root')
 );
