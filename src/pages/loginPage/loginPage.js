78c4516a7f82d385a2ccd35af5da71b94f46c3f0
vika <vihka.spihka@gmail.com>
2017-04-23 03:43:34 +0300

    Added data for input, routing; changed input value field, fixed readme and html

--- src/pages/loginPage/loginPage.js
+++ src/pages/loginPage/loginPage.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router';
-import Input from '../common/UI/input';
+import Input from '../../common/ui/input';
 
 export default class LoginPage extends Component {
 
@@ -9,33 +9,51 @@ export default class LoginPage extends Component {
         const loginFormData = [
             {
                 inputId: 'loginForm__email',
+                classes: {
+                    wrapper: '',
+                    label: 'col-xs-2',
+                    inputWrapper: 'col-xs-10',
+                    input: ''
+                },
                 placeholder: 'email@smth.com',
                 labelText: 'Email: ',
-                type: 'text'
+                type: 'text',
+                value: ''
             },
             {
                 inputId: 'loginForm__password',
+                classes: {
+                    wrapper: '',
+                    label: 'col-xs-2',
+                    inputWrapper: 'col-xs-10',
+                    input: ''
+                },
                 placeholder: 'Type password',
                 labelText: 'Пароль: ',
-                type: 'password'
+                type: 'password',
+                value: ''
             }
         ];
 
         return (
             <section className="row">
                 <div className="col-xs-10 col-xs-offset-1 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
-                    <form>
+                    <form className="form-horizontal">
                         <Input
                             inputId={loginFormData[0].inputId}
+                            classes={loginFormData[0].classes}
                             placeholder={loginFormData[0].placeholder}
                             labelText={loginFormData[0].labelText}
                             type={loginFormData[0].type}
+                            value={loginFormData[0].value}
                         />
                         <Input
                             inputId={loginFormData[1].inputId}
+                            classes={loginFormData[1].classes}
                             placeholder={loginFormData[1].placeholder}
                             labelText={loginFormData[1].labelText}
                             type={loginFormData[1].type}
+                            value={loginFormData[1].value}
                         />
                         <button className="btn btn-success">Войти</button>
                     </form>
