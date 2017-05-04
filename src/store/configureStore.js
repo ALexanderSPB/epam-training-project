57b8ea3283a2ef06e660d5d3ba8998415f7975fa
vika <vihka.spihka@gmail.com>
2017-05-04 11:39:57 +0300

    Added style for login error, deleted persistState() for some time

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,7 +1,7 @@
 import {createStore, applyMiddleware, compose} from 'redux';
 import createLogger from 'redux-logger';
 import thunkMiddleware from 'redux-thunk';
-import persistState from 'redux-localstorage';
+//import persistState from 'redux-localstorage';
 import {rootReducer}from '../reducers';
 import {redirect} from '../middleware/redirect';
 
@@ -9,8 +9,8 @@ export default function configureStore() {
     const store = compose(
         applyMiddleware(thunkMiddleware),
         applyMiddleware(createLogger()),
-        applyMiddleware(redirect),
-        persistState()
+        applyMiddleware(redirect)
+        // persistState()
     )(createStore)(rootReducer);
 
     if (module.hot) {
