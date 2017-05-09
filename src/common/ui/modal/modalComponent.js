336874016612664c7f110af1ac9a956e4776bac7
Japaka90 <japaka@yandex.ru>
2017-05-09 18:37:26 +0300

    created common modal component

--- src/common/ui/modal/modalComponent.js
+++ src/common/ui/modal/modalComponent.js
@@ -1,69 +1,77 @@
 import React, { Component } from 'react';
 
-export default class ModalComponent extends Component {
+export default class Modal extends Component {
 
-    componentWillMount() {
+    static defaultProps = {
+        visible: false,
+        openButtonTitle: 'Открыть модальное окно',
+        cancelButtonTitle: 'Закрыть',
+        actionButtonTitle: 'ОК',
+        title: '',
+        text: '',
+        submitAction: function() {return null}
+    };
+
+    constructor(props) {
+        super(props);
         this.state = {
-            visible: this.props.visible ? this.props.visible : true,
-            modal_header: this.props.modal_header ? this.props.modal_header : '',
-            modal_content: this.props.modal_content ? this.props.modal_content : '',
-            cancel_title: this.props.cancel_title ? this.props.cancel_title : 'Закрыть',
-            action_title: this.props.action_title ? this.props.action_title : 'ОК'
+            visible: this.props.visible,
+            openButtonTitle: this.props.openButtonTitle,
+            cancelButtonTitle: this.props.cancelButtonTitle,
+            actionButtonTitle: this.props.actionButtonTitle,
+            title: this.props.title,
+            text: this.props.text,
+            submitAction: this.props.submitAction
         };
     }
 
-
-    closeModal() {
-        this.setState({
-            visible: false
-        });
+    toggleModal() {
+        this.setState({ visible: !this.state.visible });
     }
+
 
     actionModal() {
-        this.setState({
-            visible: false
-        });
+        this.toggleModal();
+        this.state.submitAction();
     }
 
-    openModal() {
-        this.setState({
-            visible: true
-        });
-
-    }
 
     render() {
-        let modalClass = this.state.visible ? 'modal fade in' : 'modal fade';
-        let modalStyles = this.state.visible ? {display: 'block'} : {};
+        let modalClass = this.state.visible ? "modal fade in" : "modal fade";
+        let modalStyles = this.state.visible ? {display: "block"} : {};
         let backdrop = this.state.visible ? (
-                <div className='modal-backdrop fade in' onClick={this.close} />
+                <div className="modal-backdrop fade in" data-backdrop="static"/>
             ) : null;
-        let modal_header = this.state.modal_header ? (
-                <div className='modal-header'>
-                    <h4 className='modal-title'>{this.state.modal_header}</h4>
+
+        let title = this.state.title ? (
+                <div className="modal-header">
+                    <h4 className="modal-title">{this.state.title}</h4>
                 </div>
             ) : null;
 
         return (
-            <div className={modalClass} style={modalStyles}>
-                {backdrop}
-                <div className='modal-dialog'>
-                    <div className='modal-content'>
-                        {modal_header}
-                        <div className='modal-body'>
-                            <p>{this.state.modal_content}</p>
-                        </div>
-                        <div className='modal-footer'>
-                            <button type='button' className='btn btn-default'
-                                    onClick={this.close}>{this.state.cancel_title}</button>
-                            <button type='button' className='btn btn-primary'
-                                    onClick={this.action}>{this.state.action_title}</button>
+            <div>
+                <button onClick={this.toggleModal.bind(this)}>{this.state.openButtonTitle}</button>
+
+                <div className={modalClass} style={modalStyles}>
+
+                    <div className="modal-dialog">
+                        <div className="modal-content">
+                            {title}
+                            <div className="modal-body">
+                                <p>{this.state.text}</p>
+                            </div>
+                            <div className="modal-footer">
+                                <button type="button" className="btn btn-default"
+                                        onClick={this.toggleModal.bind(this)}>{this.state.cancelButtonTitle}</button>
+                                <button type="button" className="btn btn-primary"
+                                        onClick={this.actionModal.bind(this)}>{this.state.actionButtonTitle}</button>
+                            </div>
                         </div>
                     </div>
                 </div>
+                {backdrop}
             </div>
-        );
+        )
     }
-
-
 }
