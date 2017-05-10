40d752783d2d99bbe201f4a203ab3784f5a92e68
vika <vihka.spihka@gmail.com>
2017-05-10 20:22:27 +0300

    Made Header and Footer stateless

--- src/common/parts/footer/footer.js
+++ src/common/parts/footer/footer.js
@@ -1,31 +1,29 @@
-import React, {Component} from 'react';
+import React from 'react';
 import {browserHistory} from 'react-router';
 import {ROUTE_PATHS} from '../../../constants/routes';
 
-class Footer extends Component {
+const Footer = () => {
 
-    goToPage(page) {
+    function goToPage(page) {
         browserHistory.push(page);
     }
 
-    render() {
-        return(
-            <footer className="app_footer">
-                <ul className="app_footer_content">
-                    <li>
-                        <a onClick={() => this.goToPage(ROUTE_PATHS.root)}>
-                            Главная
-                        </a>
-                    </li>
-                    <li>
-                        <a onClick={() => this.goToPage(ROUTE_PATHS.faq)}>
-                            FAQ
-                        </a>
-                    </li>
-                </ul>
-            </footer>
-        )
-    }
+    return(
+        <footer className="app_footer">
+            <ul className="app_footer_content">
+                <li>
+                    <a onClick={() => goToPage(ROUTE_PATHS.root)}>
+                        Главная
+                    </a>
+                </li>
+                <li>
+                    <a onClick={() => goToPage(ROUTE_PATHS.faq)}>
+                        FAQ
+                    </a>
+                </li>
+            </ul>
+        </footer>
+    );
 }
 
 export default Footer;
--- src/common/parts/header/header.js
+++ src/common/parts/header/header.js
@@ -1,37 +1,35 @@
-import React, {Component} from 'react';
+import React from 'react';
 import {browserHistory} from 'react-router';
 import favicon from '../../../../public/favicon.ico';
 import './headerStyle.css';
 import {ROUTE_PATHS} from '../../../constants/routes';
 
-class Header extends Component {
+const Header = () => {
 
-    goToPage(page) {
+    function goToPage(page) {
         browserHistory.push(page);
     }
 
-    render() {
-        return(
-            <header className="app_header row">
-                <div className="app_header_logo_wrapper col-xs-3">
-                    <a onClick={() => this.goToPage(ROUTE_PATHS.root)}>
-                        <img src={favicon} alt="logo"/>
-                    </a>
-                </div>
-                <div className="col-xs-6">
-                    <h1>MySchedule</h1>
-                </div>
-                <div className="app_header_buttons col-xs-3">
-                    <button onClick={() => this.goToPage(ROUTE_PATHS.login)} className="btn">
-                        Войти
-                    </button>
-                    <button onClick={() => this.goToPage(ROUTE_PATHS.registration)} className="btn">
-                        Зарегистрироваться
-                    </button>
-                </div>
-            </header>
-        );
-    }
+    return(
+        <header className="app_header row">
+            <div className="app_header_logo_wrapper col-xs-3">
+                <a onClick={() => goToPage(ROUTE_PATHS.root)}>
+                    <img src={favicon} alt="logo"/>
+                </a>
+            </div>
+            <div className="col-xs-6">
+                <h1>MySchedule</h1>
+            </div>
+            <div className="app_header_buttons col-xs-3">
+                <button onClick={() => goToPage(ROUTE_PATHS.login)} className="btn">
+                    Войти
+                </button>
+                <button onClick={() => goToPage(ROUTE_PATHS.registration)} className="btn">
+                    Зарегистрироваться
+                </button>
+            </div>
+        </header>
+    );
 }
 
 export default Header;
