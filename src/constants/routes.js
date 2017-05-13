ce688682d672021620087556448b68610184d702
Dinulkin <undinakamec@gmail.com>
2017-05-13 17:00:32 +0300

    - text added
    -style
    
    -added component Schedule,
    -officeHours fixed
    
    -institutionAndGroupScheduleSelector moved to mainPage
    -teachers.js removed

--- src/constants/routes.js
+++ src/constants/routes.js
@@ -6,17 +6,16 @@ import MainPage from '../pages/mainPage/mainPage';
 import LoginPage from '../pages/loginPage/loginPage';
 import RegistrationPage from '../pages/registrationPage/registrationPage';
 import FaqPage from '../pages/faqPage/faqPage';
+import TeacherWorkflow from '../pages/teacherWorkflow/teacherWorkflow';
 import NotFound from '../pages/notFound/notFound';
-import WorkflowTeacher from '../pages/workflowTeacher/workflowTeacher';
 
 export const ROUTE_PATHS = {
     root: '/',
     login: 'login',
     registration: 'registration',
     faq: 'faq',
+    teacher: 'teacher',
     notFound: '*'
-    login: 'login',
-    teacher: 'teacher'
 };
 
 export const routes = (
@@ -25,8 +24,7 @@ export const routes = (
         <Route path={ROUTE_PATHS.login} component={ LoginPage } />
         <Route path={ROUTE_PATHS.registration} component={ RegistrationPage } />
         <Route path={ROUTE_PATHS.faq} component={ FaqPage } />
+        <Route path={ROUTE_PATHS.teacher} component={ TeacherWorkflow } />
         <Route path={ROUTE_PATHS.notFound} component={ NotFound } />
-        <Route path="registration" component={ RegistrationPage } />
-        <Route path={ROUTE_PATHS.teacher} component={ WorkflowTeacher } />
     </Route>
 );
