fda9f8ed2271d76664876c8b31b74e0149c62ece
Dina Mukhametshina <undinakamec@gmail.com>
2017-05-14 11:55:29 +0300

    teacherCard - done
    added teacherCard
    added HOC for teacherCard
    
    (cherry picked from commit 69ff91f3d83f20a58f11d8ec6d3177f7882684a8)

---
+++ src/pages/managerWorkflow/teacherSection/teacherSection.js
@@ -0,0 +1,50 @@
+import React, {Component} from 'react';
+import {connect} from 'react-redux';
+import PropTypes from 'prop-types';
+import {bindActionCreators} from 'redux';
+import {fetchEntities} from '../../../common/ui/institutionAndGroupScheduleSelector/fetchEntityActions';
+import TeacherCard from '../../managerWorkflow/teacherSection/teacherCard/teacherCard';
+import {TEACHERS} from '../../../constants/fetchActionsTypes';
+import {PATHS} from '../../../constants/database';
+import Firebase from '../../../common/helpers/firebase';
+
+
+const mapStateToProps = state => ({
+    teachers: state.teachers,
+});
+
+const mapDispatchToProps = dispatch => ({
+    fetchEntities: bindActionCreators(fetchEntities, dispatch),
+    dispatch,
+});
+
+class TeacherSection extends Component {
+    constructor(props) {
+        super(props);
+        const {users} = PATHS;
+        props.fetchEntities(users, TEACHERS);
+        this.state = {};
+        Firebase.get(PATHS.skills)
+            .then(skills => this.setState({skills}));
+    }
+
+    render() {
+        const {teachers} = this.props;
+        const {skills} = this.state;
+        return (
+            <section className="row">
+                {
+                    (skills === undefined) ? null :
+                    teachers.map(teacher => ({...teacher, skills: teacher.skills.map(skillID => skills[skillID])}))
+                        .map(teacher => <TeacherCard key={teacher.uuid} {...teacher}/>)}
+            </section>
+        );
+    }
+}
+
+TeacherSection.propTypes = {
+    fetchEntities: PropTypes.func,
+    teachers: PropTypes.array,
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(TeacherSection);
