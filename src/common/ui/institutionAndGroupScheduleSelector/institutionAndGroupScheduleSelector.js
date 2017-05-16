df68dd2f1bc0b9b1b23fa5f835acfb92d20050e8
Alanolmo <kat.biserova@gmail.com>
2017-05-16 16:43:11 +0300

    Scroll only after schedule is loaded. Scroll only to the top of the schedule

--- src/common/ui/institutionAndGroupScheduleSelector/institutionAndGroupScheduleSelector.js
+++ src/common/ui/institutionAndGroupScheduleSelector/institutionAndGroupScheduleSelector.js
@@ -8,6 +8,7 @@ import {fetchEntities} from './fetchEntityActions';
 import {PATHS} from '../../../constants/database';
 import {INSTITUTIONS, GROUPS} from '../../../constants/fetchActionsTypes';
 import Firebase from '../../helpers/firebase';
+import {scheduleCellHeight} from '../../../constants/scheduleOptions';
 
 const mapStateToProps = state => ({
     'institutions': state.institutions,
@@ -36,6 +37,7 @@ class InstitutionAndGroupScheduleSelector extends Component {
 
     handleSelectInstitution(selected) {
         this.setState({selectedInstitution: selected});
+        this.setState({institutionInfo: this.props.institutions.find(x => x.uuid === selected)});
     }
 
     handleSelectGroup(selected) {
@@ -51,14 +53,16 @@ class InstitutionAndGroupScheduleSelector extends Component {
     }
 
     componentDidUpdate() {
-        window.scrollTo(0 , document.body.offsetHeight);
+        if (this.state.events === undefined)
+            return;
+        const timing = this.state.institutionInfo.timing;
+        let scheduleHeight = (timing.closing - timing.opening + 3) * scheduleCellHeight;
+        window.scrollTo(0 , document.body.offsetHeight - scheduleHeight);
     }
 
     render() {
-        const {selectedInstitution, events} = this.state;
+        const {selectedInstitution, events, institutionInfo} = this.state;
         const {institutions, groups} = this.props;
-
-        let institutionInfo = institutions.find(x => x.uuid === selectedInstitution);
 
         return (
             <div>
