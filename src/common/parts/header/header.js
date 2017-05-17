9e376a43d69e9262b0ca35bcd898fd972dd26194
AlexanderSPB <aburameq@gmail.com>
2017-05-17 23:15:23 +0300

    Header Change

--- src/common/parts/header/header.js
+++ src/common/parts/header/header.js
@@ -1,41 +1,88 @@
 import React from 'react';
 import {browserHistory} from 'react-router';
+import {Component} from 'react';
+import {bindActionCreators} from 'redux';
+import {connect} from 'react-redux';
+import {Link} from 'react-router';
 import logo from '../../../../public/img/logo.png';
 import {ROUTE_PATHS} from '../../../constants/routes';
+import * as headerActions from './headerAction';
 import './header.css';
 
-const Header = () => {
+class Header extends Component {
 
-    function goToPage(page) {
+    constructor(props, context){
+        super(props, context);
+        this.handleClick = this.handleClick.bind(this);
+    }
+
+    goToPage(page) {
         browserHistory.push(page);
     }
 
-    return (
-        <header className="app-header row">
+    handleClick(e) {
+        e.preventDefault();
+        this.props.headerActions.logOut();
+    }
+    loggedIn() {
+        return(
             <div
-                className="app-header__logo-wrapper col-xs-2 col-sm-2 col-md-1 col-md-offset-0">
-                <a onClick={() => goToPage(ROUTE_PATHS.root)} className="navbar-brand">
-                    <img src={logo} alt="logo" />
-                </a>
+                className="app-header__buttons">
+                <h4 onClick={() => this.goToPage(ROUTE_PATHS.root)} className="my-schedule__header">
+                    Greetings, {this.props.userName}
+                </h4>
+                <button className="button log-out__button" onClick={this.handleClick}>
+                    <span className="button__text">Log Out</span>
+                    <span className="glyphicon glyphicon-edit button__glyph"/>
+                </button>
             </div>
-            <div className="col-xs-4 col-sm-4 col-md-3">
-                <h1 onClick={() => goToPage(ROUTE_PATHS.root)} className="my-schedule__header">
-                    My&nbsp;Schedule
-                </h1>
-            </div>
+        )
+    }
+    loggedOut(){
+        return(
             <div
                 className="app-header__buttons col-xs-4 col-xs-offset-2 col-sm-4 col-sm-offset-1 col-md-3 col-md-offset-4">
-                <button onClick={() => goToPage(ROUTE_PATHS.login)} className="button sign-in__button">
+                <button onClick={() => this.goToPage(ROUTE_PATHS.login)} className="button sign-in__button">
                     <span className="glyphicon glyphicon-log-in button__glyph"/>
-                    <span className="button__text">Войти</span>
+                    <span className="button__text">Log in</span>
                 </button>
-                <button onClick={() => goToPage(ROUTE_PATHS.registration)} className="button sign-up__button">
+                <button onClick={() => this.goToPage(ROUTE_PATHS.registration)} className="button sign-up__button">
                     <span className="glyphicon glyphicon-edit button__glyph"/>
-                    <span className="button__text">Зарегистрироваться</span>
+                    <span className="button__text">Sign up</span>
                 </button>
             </div>
-        </header>
-    );
-}
+        )
+    }
 
-export default Header;
+    render() {
+        return (
+            <header className="app-header row">
+                <div
+                    className="app-header__logo-wrapper col-xs-2 col-sm-2 col-md-1 col-md-offset-0">
+                    <a onClick={() => this.goToPage(ROUTE_PATHS.root)} className="navbar-brand">
+                        <img src={logo} alt="logo"/>
+                    </a>
+                </div>
+                <div className="col-xs-4 col-sm-4 col-md-3">
+                    <h4 onClick={() => this.goToPage(ROUTE_PATHS.root)} className="my-schedule__header">
+                        My&nbsp;Schedule
+                    </h4>
+                </div>
+                { this.props.userName !== '' && this.loggedIn() }
+                { this.props.userName === '' && this.loggedOut() }
+            </header>
+        );
+    }
+}
+function mapStatetoProps(state) {
+    if (state.loginData.user === undefined) state.loginData.user = {name: ''};
+    return{
+        userName: state.loginData.user.name
+    }
+}
+function mapDispatchtoProps(dispatch) {
+    return{
+        headerActions: bindActionCreators(headerActions, dispatch)
+    }
+}
+export default connect(mapStatetoProps, mapDispatchtoProps)(Header);
--- src/pages/loginPage/loginReducer.js
+++ src/pages/loginPage/loginReducer.js
@@ -1,5 +1,5 @@
 import * as actionTypes from './loginActions';
-
+import * as headerActionTypes from '../../common/parts/header/headerAction';
 class LoginData {
     constructor({ name, uid, role }) {
         this.name = name;
@@ -13,6 +13,8 @@ const initialState = {};
 export default function loginData(state = initialState, action) {
     const {payload, type} = action;
     switch (type) {
+        case headerActionTypes.LOG_OUT:
+            return initialState;
         case actionTypes.LOGIN_SUCCESS:
             return {
                 user: new LoginData(payload)
