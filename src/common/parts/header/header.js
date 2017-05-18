5e9d621a265411104219cfb4ab32511a343d6820
AlexanderSPB <aburameq@gmail.com>
2017-05-18 18:11:24 +0300

    Header Change

--- src/common/parts/header/header.js
+++ src/common/parts/header/header.js
@@ -38,14 +38,18 @@ class Header extends Component {
     loggedOut(){
         return (
             <div className="app-header__buttons col-xs-4 col-xs-offset-2 col-sm-4 col-sm-offset-1 col-md-3 col-md-offset-4">
+                <Link className='link' to={ROUTE_PATHS.login}>
                 <button className="button sign-in__button">
                     <span className="glyphicon glyphicon-log-in button__glyph"/>
-                    <Link to={ROUTE_PATHS.login}><span className="button__text">Log in</span></Link>
+                    <span className="button__text">Log in</span>
                 </button>
+                </Link>
+                <Link className='link' to={ROUTE_PATHS.registration}>
                 <button className="button sign-up__button">
                     <span className="glyphicon glyphicon-edit button__glyph"/>
-                    <Link to={ROUTE_PATHS.registration}><span className="button__text">Sign up</span> </Link>
+                    <span className="button__text">Sign up</span>
                 </button>
+                </Link>
             </div>
         )
     }
