7a735297d7ca37f7489b52e1530d5fbfa894f537
AlexanderSPB <aburameq@gmail.com>
2017-05-24 00:41:43 +0300

    ManagerWorkflow Schedule

--- src/pages/managerWorkflow/workflow.js
+++ src/pages/managerWorkflow/workflow.js
@@ -1,8 +1,13 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import ScheduleSection from './scheduleSection/scheduleSection';
 
 
-const Workflow = (props) => <section className="col-xs-8"><h1>{props.route.path}</h1></section>;
+const Workflow = (props) => <section className="col-xs-8">
+                                {props.route.path === 'schedule' ?
+                                    <ScheduleSection/> :
+                                    <h1>{props.route.path}</h1>}
+                            </section>;
 
 Workflow.propTypes = {
     route: PropTypes.object.isRequired,
